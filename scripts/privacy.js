c011d8322308f7d2c5408e41f78946560381f0c0
markiewiczpiotr85@gmail.com <markiewiczpiotr85@gmail.com>
2025-02-23 20:14:09 +0100

    fix policy

--- scripts/privacy.js
+++ scripts/privacy.js
@@ -1,12 +1,15 @@
 const cookieBox = document.querySelector('.cookie-box')
 const cookieBtn = document.querySelector('.cookie-btn')
-
+const cancelBtn = document.querySelector('.cancel-btn')
 
 const showCookie = () => {
 	const cookieEaten = localStorage.getItem('cookie')
+	const cookieCancelled = localStorage.getItem('cookie-cancelled')
 
 	if (cookieEaten) {
 		cookieBox.classList.add('hide')
+	} else if (cookieCancelled) {
+		cookieBox.classList.remove('hide')
 	}
 }
 
@@ -16,5 +19,11 @@ const handleCookieBox = () => {
 	cookieBox.classList.add('hide')
 }
 
+const handleCancel = () => {
+	localStorage.setItem('cookie-cancelled', 'true')
+	cookieBox.classList.add('hide')
+}
+
 cookieBtn.addEventListener('click', handleCookieBox)
+cancelBtn.addEventListener('click', handleCancel)
 showCookie()
